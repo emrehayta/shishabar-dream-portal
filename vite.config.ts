af08f4911a7dcb674648e6690ef1677172b82e58
Emre Hayta <emrehayta21@gmail.com>
2025-07-27 17:01:53 +0200

    new change

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,38 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   base: '/shishabar-dream-portal/' // Replace with your GitHub repo name
+// }));
+
+
 export default defineConfig(({ mode }) => ({
+  base: '/', // Root statt Unterordner
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/shishabar-dream-portal/' // Replace with your GitHub repo name
 }));
